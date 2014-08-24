513322ef828485be9639b298b7afcd6dcf99c444
Samy Pessé <samypesse@gmail.com>
2014-08-23 17:27:11 -0700

    Add base representation for glossary

---
+++ src/collections/glossary.js
@@ -0,0 +1,38 @@
+define([
+    "hr/hr",
+    "hr/utils",
+    "models/glossaryEntry"
+], function(hr, _, GlossaryEntry) {
+    var parseGlossary = node.require("gitbook").parse.glossary;
+
+    var Glossary = hr.Collection.extend({
+        model: GlossaryEntry,
+
+        /*
+         *  Parse GLOSSARY.md content to extract entries
+         */
+        parseGlossary: function(content) {
+            var entries = parseGlossary(content);
+
+            try {
+                this.reset(entries);
+            } catch (e) {
+                console.error(e.stack);
+            }
+
+        },
+
+        /*
+         *  Return a markdown representation of the glossary
+         */
+        toMarkdown: function() {
+            var bl = "\n";
+
+            return this.map(function(entry) {
+                return "## "+entry.name+bl+bl+entry.description;
+            }).join(bl+bl);
+        }
+    });
+
+    return Glossary;
+});
