7579d4d6e3ef3816322df4706bc96f28ea03c678
Samy Pessé <samypesse@gmail.com>
2014-09-06 22:51:04 +0200

    Use gitbook.io for checking version

--- src/core/update.js
+++ src/core/update.js
@@ -1,28 +1,21 @@
 define([
-    "hr/promise"
-], function(Q) {
+    "hr/promise",
+    "core/gitbookio"
+], function(Q, gitbookIO) {
     var request = node.require("request");
     var semver = node.require("semver");
     var pkg = node.require("../package.json");
     var gitbookPkg = node.require("gitbook/package.json");
 
     var isAvailable = function() {
-        var d = Q.defer();
-        request({
-            url: "https://raw.github.com/GitbookIO/editor/master/package.json",
-            json: true
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                if (body && body.version && semver.gt(body.version, pkg.version)) {
-                    d.resolve(body.version);
-                } else {
-                    d.reject(new Error("no updates"));
-                }
+        return gitbookIO.api.request("get", "editor/infos")
+        .then(function(infos) {
+            if (semver.gt(infos.version, pkg.version)) {
+                return body.version;
             } else {
-                d.reject(error);
+                return Q.reject(new Error("no updates"));
             }
         });
-        return d.promise;
     };
 
     return {
